0b5f29f7776d3a068ff210ed8b6a44f09315a1ab
Mrinmay Santra <mrinmay7875@gmail.com>
2021-02-18 20:44:32 +0530

    Added contact me button,increased profile photo size,updated UI

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,19 +1,23 @@
 import React, { Component } from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import { FaPaperPlane } from "react-icons/fa";
 
 class About extends Component {
   render() {
     if (this.props.data) {
-      var name = this.props.data.name;
+      // var name = this.props.data.name;
       var profilepic = "images/" + this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone = this.props.data.phone;
-      var email = this.props.data.email;
-      // var resumeDownload = this.props.data.resumedownload;
+      var bio_line1 = this.props.data.bio_line1;
+      var bio_line2 = this.props.data.bio_line2;
+      var bio_line3 = this.props.data.bio_line3;
+      var bio_line4 = this.props.data.bio_line4;
+      // var bio_array = bio.split("/");
+      // var street = this.props.data.address.street;
+      // var city = this.props.data.address.city;
+      // var state = this.props.data.address.state;
+      // var zip = this.props.data.address.zip;
+      // var phone = this.props.data.phone;
+      // var email = this.props.data.email;
     }
 
     return (
@@ -28,26 +32,22 @@ class About extends Component {
           </div>
           <div className="nine columns main-col">
             <h2>About Me</h2>
-
-            <p>{bio}</p>
-            <div className="row">
-              <div className="columns contact-details">
-                <h2>Contact Details</h2>
-                <p className="address">
-                  <span>{name}</span>
-                  <br />
-                  <span>
-                    {street}
-                    <br />
-                    {city} {state}, {zip}
-                  </span>
-                  <br />
-                  <span>{phone}</span>
-                  <br />
-                  <span>{email}</span>
-                </p>
-              </div>
-              <div className="columns download">
+            <ol style={{ listStyle: "square", color: "#e7e6e1" }}>
+              <li style={{ paddingRight: "60px", marginBottom: "30px" }}>
+                {bio_line1}
+              </li>
+              <li style={{ paddingRight: "20px", marginBottom: "30px" }}>
+                {bio_line2}
+              </li>
+              <li style={{ paddingRight: "20px", marginBottom: "30px" }}>
+                {bio_line3}
+              </li>
+              <li style={{ paddingRight: "20px", marginBottom: "30px" }}>
+                {bio_line4}
+              </li>
+            </ol>
+            <div className="">
+              <div className="columns download ">
                 <p>
                   <a
                     href="https://drive.google.com/file/d/1leRTZQkeLDNnqJ2SGNyPTyYw0RZur9S-/view?usp=sharing"
@@ -57,6 +57,20 @@ class About extends Component {
                   >
                     View Resume
                     <FaExternalLinkAlt style={{ marginLeft: "6px" }} />
+                  </a>
+                </p>
+              </div>
+              {/* Contact me button */}
+              <div className="columns contact-btn ">
+                <p>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="button smoothscroll"
+                    href="#contact"
+                  >
+                    Contact Me
+                    <FaPaperPlane style={{ marginLeft: "10px" }} />
                   </a>
                 </p>
               </div>
