eee8391eede1e3ecf22b088f644c9358d5dc0e5f
Mrinmay Santra <mrinmay7875@gmail.com>
2021-03-28 19:13:46 +0530

    Updated resume link

--- src/Components/About.js
+++ src/Components/About.js
@@ -42,7 +42,7 @@ class About extends Component {
               <div className="columns download ">
                 <p>
                   <a
-                    href="https://drive.google.com/file/d/1leRTZQkeLDNnqJ2SGNyPTyYw0RZur9S-/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1PUmM1VRl7DzlhgnvN9GfFnkZuV_-h7jD/view?usp=sharing"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="button"
